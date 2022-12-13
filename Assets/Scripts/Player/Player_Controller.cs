a866053fa3bbd7b9fe5c474e2940cce9d7637d5c
Figureeeeee <1157552798@qq.com>
2022-12-13 14:01:22 +0800

    Player_Input Audio

---
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : FSMController
+{
+    public override Enum CurrentState { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    private Player_Input input;
+    private new Player_Audio audio;
+
+    private void Start()
+    {
+        input = new Player_Input();
+        audio = new Player_Audio(GetComponent<AudioSource>());
+    }
+}
